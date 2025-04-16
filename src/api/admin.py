d5fd7b49e7fe93bc7d79cf3977dfcf1acbe538b1
Federico Serron Umpierrez <77068927+federico-serron@users.noreply.github.com>
2025-04-16 22:40:43 +0000

    All models for database have been created and their relationships. Also pipfile updated to use to last sqlalchemy version

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,7 +1,7 @@
   
 import os
 from flask_admin import Admin
-from .models import db, User
+from .models import db, User, Record, Category, Currency, Wallet
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
@@ -12,6 +12,11 @@ def setup_admin(app):
     
     # Add your models here, for example this is how we add a the User model to the admin
     admin.add_view(ModelView(User, db.session))
+    admin.add_view(ModelView(Record, db.session))
+    admin.add_view(ModelView(Category, db.session))
+    admin.add_view(ModelView(Currency, db.session))
+    admin.add_view(ModelView(Wallet, db.session))
+
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
