bee25e398697303ce93f745f5a7536b3262d5ef0
rvarga09 <146484780+rvarga09@users.noreply.github.com>
2025-04-21 00:14:58 +0000

    dashboardv2

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -2,7 +2,7 @@ import React, { useContext, useState } from "react";
 import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
-import { Dashboard } from "../component/Dashboard.jsx";
+
 
 
 
@@ -10,8 +10,8 @@ import { Dashboard } from "../component/Dashboard.jsx";
 export const Home = () => {
 	return (
 		<div>
-
-			<Dashboard/>
+			
+			pagina de inicio
 			
 		</div>
 	)
