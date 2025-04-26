2599f3d99526f33904f533ddab16bc3cf8c94c42
Federico Serron Umpierrez <77068927+federico-serron@users.noreply.github.com>
2025-04-26 11:33:50 +0000

    pequenos cambios

--- src/front/js/component/AddRecord.jsx
+++ src/front/js/component/AddRecord.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import ListRecord from "./ListRecord.jsx";
 import { Context } from "../store/appContext";
 
@@ -6,8 +6,7 @@ import { Context } from "../store/appContext";
 const AddRecord = () => {
     const { store, actions } = useContext(Context);
     const [inputValue, setInputValue] = useState("");
-    const [assignedCategory, setAssignedCategory] = useState(null)
-
+    const [totalValueWallet, setTotalValueWallet] = useState(0)
 
     const removeAccents = (str) => {
         return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
@@ -25,47 +24,63 @@ const AddRecord = () => {
         const amountMatch = inputValue.match(/-?\d+(\.\d+)?/);
         const amount = amountMatch ? parseFloat(amountMatch[0]) : null;
         const words = inputValue
-            .replace(/-?\d+(\.\d+)?/, "")
+            .replace(/[+-]?\d+(\.\d+)?/, "")
             .toLowerCase()
             .split(/\s+/)
             .map(removeAccents)
             .map(singularize);
 
+            let matchedCategory = null;
+
         for (const [category, keywords] of Object.entries(categories)) {
             if (words.some(word => keywords.includes(word))) {
-                setAssignedCategory(category);
+                matchedCategory = category;
                 break;
             }
         }
 
-        console.log(amount, assignedCategory, words.join(" "))
+        console.log(amount, matchedCategory, words.join(" "))
         setInputValue("")
         return store.records = [...store.records, {
             id: store.records.length + 1,
             description: words.join(" "),
             amount: amount,
             category:{
-                name: assignedCategory
+                name: matchedCategory
             }
         }];
     };
 
+    useEffect(()=>{
+        let totalValue = 0
+        for (let i = 0; i < store.records.length; i++) {
+            totalValue = totalValue + store.records[i].amount;
+        }
+        setTotalValueWallet(totalValue)
+        console.log(totalValue)
+    }, [store.records])
+
     return (
-        <div className="container mt-4 col-4">
-            <ListRecord />
-            <h5 className="my-3">Ingresa tu registro</h5>
-            <div className="input-group">
-                <button onClick={() => { handleRecord(inputValue, store.categories) }} className="btn btn-custom" type="button">
-                    ➕
-                </button>
-                <input
-                    onChange={(e) => { setInputValue(e.target.value) }}
-                    onKeyDown={(e)=>{e.key === "Enter" ? handleRecord(inputValue, store.categories) : ""}}
-                    type="text"
-                    className="form-control"
-                    placeholder="-250 comida con la banda"
-                    value={inputValue}
-                />
+        <div className="container mt-4">
+            <div className="row d-flex">
+                <ListRecord />
+                <h3 className="mt-3">Tienes ${totalValueWallet} disponibles</h3>
+            </div>
+            <div className="row d-flex">
+                <h5 className="my-3">Ingresa tu registro</h5>
+                <div className="input-group col-4">
+                    <button onClick={() => { handleRecord(inputValue, store.categories) }} className="btn btn-custom" type="button">
+                        ➕
+                    </button>
+                    <input
+                        onChange={(e) => { setInputValue(e.target.value) }}
+                        onKeyDown={(e)=>{e.key === "Enter" ? handleRecord(inputValue, store.categories) : ""}}
+                        type="text"
+                        className="form-control"
+                        placeholder="-250 comida con la banda"
+                        value={inputValue}
+                    />
+                </div>
             </div>
         </div>
     )
